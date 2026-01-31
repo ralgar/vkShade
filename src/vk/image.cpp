593aaee164e7eea72d533a5d0ac6f2a48f498e00
Ryan Algar <me@ralgar.dev>
2026-01-30 21:36:06 -0800

    feat(vulkan): implement VulkanImage class

---
+++ src/vk/image.cpp
@@ -0,0 +1,173 @@
+#include "image.hpp"
+#include "macros.hpp"
+#include <vulkan/vulkan_core.h>
+
+vkShade::VulkanImage::VulkanImage(VulkanDevice& device, VkExtent2D size, VkFormat format, VkImageUsageFlags usageFlags)
+    : VulkanObject(device)
+{
+    spdlog::trace("Creating image (Size: {}x{}, Format: {})",
+        size.width, size.height, magic_enum::enum_name(format));
+
+    // Set internal properties
+    m_extent = size;
+    m_format = format;
+    m_usageFlags = usageFlags;
+
+    this->create_image();
+    this->create_image_view();
+}
+
+vkShade::VulkanImage::VulkanImage(VulkanDevice& device, VkImage image, VkExtent2D size, VkFormat format)
+    : VulkanObject(device)
+{
+    spdlog::trace("Wrapping existing image (Size: {}x{}, Format: {})",
+        size.width, size.height, magic_enum::enum_name(format));
+
+    // Set internal properties
+    m_image = image;
+    m_extent = size;
+    m_format = format;
+    m_owning = false;  // IMPORTANT! We don't own the image here.
+
+    // Only create the view (no image since we don't own it)
+    this->create_image_view();
+}
+
+vkShade::VulkanImage::~VulkanImage()
+{
+    spdlog::trace("Destroying VulkanImage");
+
+    if (m_imageView != VK_NULL_HANDLE)
+        m_device.dispatch.DestroyImageView(m_device.handle, m_imageView, nullptr);
+
+    if (m_owning && m_image != VK_NULL_HANDLE && m_allocation != VK_NULL_HANDLE)
+        vmaDestroyImage(m_device.allocator, m_image, m_allocation);
+}
+
+void vkShade::VulkanImage::create_image()
+{
+    VkImageCreateInfo imgInfo = {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        .pNext = nullptr,
+        .imageType = VK_IMAGE_TYPE_2D,
+        .format = m_format,
+        .extent = VkExtent3D {m_extent.width, m_extent.height, 1},
+        .mipLevels = 1,
+        .arrayLayers = 1,
+        .samples = VK_SAMPLE_COUNT_1_BIT,   // 1 sample per pixel = no MSAA
+        .tiling = VK_IMAGE_TILING_OPTIMAL,  // Let the GPU move the data as it sees fit
+        .usage = m_usageFlags,
+    };
+
+	// Always allocate images on dedicated GPU memory.
+	VmaAllocationCreateInfo allocInfo = {};
+	allocInfo.usage = VMA_MEMORY_USAGE_GPU_ONLY;
+	allocInfo.requiredFlags = VkMemoryPropertyFlags(VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+
+	// Allocate and create the image.
+	VK_CHECK(vmaCreateImage(m_device.allocator, &imgInfo, &allocInfo, &m_image, &m_allocation, nullptr));
+}
+
+void vkShade::VulkanImage::create_image_view()
+{
+    // If the format is a depth format, we will need to have it use the correct aspect flag.
+	VkImageAspectFlags aspectFlags = VK_IMAGE_ASPECT_COLOR_BIT;
+	if (m_format == VK_FORMAT_D32_SFLOAT) {
+		aspectFlags = VK_IMAGE_ASPECT_DEPTH_BIT;
+	}
+
+	// Build an image view for the image.
+    VkImageViewCreateInfo viewInfo = {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+        .pNext = nullptr,
+        .image = m_image,
+        .viewType = VK_IMAGE_VIEW_TYPE_2D,
+        .format = m_format,
+        .subresourceRange = {
+            .aspectMask = aspectFlags,
+            .baseMipLevel = 0,
+            .levelCount = 1,
+            .baseArrayLayer = 0,
+            .layerCount = 1,
+        },
+    };
+
+	VK_CHECK(m_device.dispatch.CreateImageView(m_device.handle, &viewInfo, nullptr, &m_imageView));
+}
+
+void vkShade::VulkanImage::blit_from(VkCommandBuffer cmd, VkImage source)
+{
+    blit(cmd, source, m_image);
+}
+
+void vkShade::VulkanImage::blit_to(VkCommandBuffer cmd, VkImage destination)
+{
+    blit(cmd, m_image, destination);
+}
+
+void vkShade::VulkanImage::blit(VkCommandBuffer cmd, VkImage source, VkImage destination)
+{
+	VkImageBlit2 blitRegion{ .sType = VK_STRUCTURE_TYPE_IMAGE_BLIT_2, .pNext = nullptr };
+
+	blitRegion.srcOffsets[0].x = m_extent.width;
+	blitRegion.srcOffsets[0].y = m_extent.height;
+	blitRegion.srcOffsets[0].z = 1;
+
+	blitRegion.dstOffsets[0].x = m_extent.width;
+	blitRegion.dstOffsets[0].y = m_extent.height;
+	blitRegion.dstOffsets[0].z = 1;
+
+	blitRegion.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	blitRegion.srcSubresource.baseArrayLayer = 0;
+	blitRegion.srcSubresource.layerCount = 1;
+	blitRegion.srcSubresource.mipLevel = 0;
+
+	blitRegion.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	blitRegion.dstSubresource.baseArrayLayer = 0;
+	blitRegion.dstSubresource.layerCount = 1;
+	blitRegion.dstSubresource.mipLevel = 0;
+
+	VkBlitImageInfo2 blitInfo{ .sType = VK_STRUCTURE_TYPE_BLIT_IMAGE_INFO_2, .pNext = nullptr };
+	blitInfo.srcImage = m_image;
+	blitInfo.srcImageLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
+	blitInfo.dstImage = destination;
+	blitInfo.dstImageLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+	blitInfo.filter = VK_FILTER_LINEAR;
+	blitInfo.regionCount = 1;
+	blitInfo.pRegions = &blitRegion;
+
+	m_device.dispatch.CmdBlitImage2(cmd, &blitInfo);
+}
+
+void vkShade::VulkanImage::transition_layout(VkCommandBuffer cmd, VkImageLayout newLayout)
+{
+    VkImageMemoryBarrier2 imageBarrier = {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2,
+        .srcStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
+        .srcAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT,
+        .dstStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
+        .dstAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT | VK_ACCESS_2_MEMORY_READ_BIT,
+        .oldLayout = m_currentLayout,  // Track current layout
+        .newLayout = newLayout,
+        .image = m_image,
+        .subresourceRange = {
+            .aspectMask = (newLayout == VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL)
+                ? VK_IMAGE_ASPECT_DEPTH_BIT
+                : VK_IMAGE_ASPECT_COLOR_BIT,
+            .baseMipLevel = 0,
+            .levelCount = 1,
+            .baseArrayLayer = 0,
+            .layerCount = 1,
+        },
+    };
+
+    VkDependencyInfo depInfo = {
+        .sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO,
+        .imageMemoryBarrierCount = 1,
+        .pImageMemoryBarriers = &imageBarrier,
+    };
+
+    m_device.dispatch.CmdPipelineBarrier2(cmd, &depInfo);
+
+    m_currentLayout = newLayout;  // Update tracked layout
+}
---
+++ src/vk/image.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <vk_mem_alloc.h>
+#include <vulkan/vulkan_core.h>
+
+#include "object.hpp"
+
+namespace vkShade
+{
+    class VulkanImage : public VulkanObject
+    {
+    public:
+        // Owning constructor: creates image + view
+        VulkanImage(VulkanDevice& device, VkExtent2D size, VkFormat format, VkImageUsageFlags usageFlags);
+
+        // Non-owning constructor: wraps existing image + creates its own view
+        VulkanImage(VulkanDevice& device, VkImage image, VkExtent2D size, VkFormat format);
+
+        ~VulkanImage();
+
+        // Blit FROM another image INTO this image (assumes same size, mip 0)
+        void blit_from(VkCommandBuffer cmd, VkImage source);
+
+        // Blit FROM this image INTO another image (assumes same size, mip 0)
+        void blit_to(VkCommandBuffer cmd, VkImage destination);
+
+        // Transition the image from its current layout into a new layout
+        void transition_layout(VkCommandBuffer cmd, VkImageLayout newLayout);
+
+        // Getters for handles
+        const VkImage& image() const { return m_image; }
+        const VkImageView& image_view() const { return m_imageView; }
+
+    private:
+        bool              m_owning = true;
+        VkExtent2D        m_extent {0, 0};
+        VkImage           m_image = VK_NULL_HANDLE;
+        VkImageView       m_imageView = VK_NULL_HANDLE;
+        VkImageUsageFlags m_usageFlags = 0;
+        VmaAllocation     m_allocation = VK_NULL_HANDLE;
+        VkFormat          m_format = VK_FORMAT_UNDEFINED;
+        VkImageLayout     m_currentLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+
+        // The actual blit implementation
+        void blit(VkCommandBuffer cmd, VkImage src, VkImage dest);
+
+        void create_image();
+        void create_image_view();
+    };
+} // namespace vkShade
