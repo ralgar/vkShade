dbcf660025362c2f7f341ced20f3d791a13fbeb9
Ryan Algar <me@ralgar.dev>
2026-08-26 02:03:56 -0700

    feat(gui): add a logs tab with filtering

--- src/gui/windows/main_window.cpp
+++ src/gui/windows/main_window.cpp
@@ -27,6 +27,12 @@ void vkShade::MainWindow::render()
                 ImGui::EndTabItem();
             }
 
+            if (ImGui::BeginTabItem("Logs###logs"))
+            {
+                m_logPanel.render();
+                ImGui::EndTabItem();
+            }
+
             ImGui::EndTabBar();
         }
     }
