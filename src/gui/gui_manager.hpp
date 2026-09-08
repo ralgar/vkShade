4fe48f88dc89577df9c43bc6c0b00a7e4f1d4bb9
Ryan Algar <me@ralgar.dev>
2026-09-07 21:20:10 -0700

    feat(config): make font size configurable

--- src/gui/gui_manager.hpp
+++ src/gui/gui_manager.hpp
@@ -35,12 +35,17 @@ namespace vkShade
 
         void draw_cursor();
 
+        // Input event callbacks
         void on_keyboard_event(const KeyboardEvent& event);
         void on_mouse_button_event(const MouseButtonEvent& event);
         void on_mouse_motion_event(const MouseMotionEvent& event);
         void on_mouse_wheel_event(const MouseWheelEvent& event);
         void on_text_input_event(const TextInputEvent& event);
 
+        // Config callbacks
+        void on_font_size_changed(const std::string& key, uint32_t value);
+
+        void rebuild_fonts(const uint32_t size);
         void setup_dockspace();
     };
 } // namespace vkShade
